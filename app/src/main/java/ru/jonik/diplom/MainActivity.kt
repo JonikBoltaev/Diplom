08bcbbc9cef8743b4ba4a870c5169cca73a012a8
JonikBoltaev <boltaevjahongiral@gmail.com>
2022-05-01 22:09:53 +0300

    Включение view binding в проект

--- app/src/main/java/ru/jonik/diplom/MainActivity.kt
+++ app/src/main/java/ru/jonik/diplom/MainActivity.kt
@@ -2,10 +2,13 @@ package ru.jonik.diplom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.jonik.diplom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
