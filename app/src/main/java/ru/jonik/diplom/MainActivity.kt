0aeca2dc31a40632d97dd6272dc0e3f13baa2f3f
JonikBoltaev <boltaevjahongiral@gmail.com>
2022-05-06 22:13:33 +0300

    Включение view binding в проект

--- app/src/main/java/ru/jonik/diplom/MainActivity.kt
+++ app/src/main/java/ru/jonik/diplom/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import ru.jonik.diplom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    //.
+    //..
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
